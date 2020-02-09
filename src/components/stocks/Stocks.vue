2f39bc3c7741970c395f904ddbc62cc3121b5f42
Peter Krizovnik <peterkrizovnik@tests-iMac.local>
2020-02-09 07:52:47 +0000

    Start with applications

---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <app-stock v-for="stock in stocks" v-bind:key="stock.id"></app-stock>
+  </div>
+</template>
+
+<script>
+  import Stock from './Stock.vue';
+
+  export default {
+    data() {
+      return {
+        stocks: [
+          { id: 1, name: 'BMW', price: 110 },
+          { id: 2, name: 'Google', price: 200 },
+          { id: 3, name: 'Apple', price: 250 },
+          { id: 4, name: 'Twitter', price: 34 }
+        ]
+      }
+    },
+    components: {
+      appStock: Stock
+    }
+  }
+</script>
+
+<style>
+
+</style>
